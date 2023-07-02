dace3cea5917e7726f031c967028926b16a560f1
MrRobz <robinphilipthomas@outlook.in>
2023-07-03 01:32:13 +0530

    cleanup

--- src/pages/api/generate-card.tsx
+++ src/pages/api/generate-card.tsx
@@ -1,59 +1,58 @@
-/* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "@vercel/og";
-import { NextRequest } from "next/server";
-import { CSSProperties } from "react";
+import { ImageResponse } from '@vercel/og'
+import { NextRequest } from 'next/server'
+import { CSSProperties } from 'react'
 
 export const config = {
-  runtime: "edge",
-};
+  runtime: 'edge',
+}
 
 const font1 = fetch(
-  new URL("../../assets/jua-regular-webfont.woff", import.meta.url)
-).then((res) => res.arrayBuffer());
+  new URL('../../assets/jua-regular-webfont.woff', import.meta.url)
+).then((res) => res.arrayBuffer())
 
 const imageMargin = (itemLength: number, index: number): CSSProperties => {
   if (itemLength === 1) {
-    return {};
+    return {}
   }
   if (itemLength === 2) {
-    return index === 0 ? { marginRight: "-16px" } : { marginLeft: "-16px" };
+    return index === 0 ? { marginRight: '-16px' } : { marginLeft: '-16px' }
   }
   if (itemLength === 3) {
     if (index === 0) {
-      return { marginRight: "-32px" };
+      return { marginRight: '-32px' }
     }
     if (index === 2) {
-      return { marginLeft: "-32px" };
+      return { marginLeft: '-32px' }
     }
   }
 
-  return {};
-};
+  return {}
+}
 
 export default async function handler(req: NextRequest) {
   try {
-    const [font1Data] = await Promise.all([font1]);
+    const [font1Data] = await Promise.all([font1])
 
-    const { searchParams } = new URL(req.url);
+    const { searchParams } = new URL(req.url)
 
-    const company = searchParams.get("company");
-    const appreciateValue = searchParams.get("kValue");
-    const appreciateContent = searchParams.get("kContent") || "";
-    const avatarImgs = searchParams.getAll("kAvatarImg[]") || [];
+    const company = searchParams.get('company')
+    const appreciateValue = searchParams.get('kValue')
+    const appreciateContent = searchParams.get('kContent') || ''
+    const avatarImgs = searchParams.getAll('kAvatarImg[]') || []
 
-    const lettersInOneLine = 40;
-    const lines = Math.ceil(appreciateContent.length / lettersInOneLine);
-    const imgHeight = avatarImgs.length ? 300 : 0;
-    const height = 500 + imgHeight + lines * 70 + 50;
+    const lettersInOneLine = 40
+    const lines = Math.ceil(appreciateContent.length / lettersInOneLine)
+    const imgHeight = avatarImgs.length ? 300 : 0
+    const height = 500 + imgHeight + lines * 70 + 50
 
     return new ImageResponse(
       (
         <div
           tw="w-full h-full flex"
           style={{
-            width: "100%",
-            height: "100%",
-            backgroundColor: "#EBEBEB",
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#EBEBEB',
           }}
         >
           <div tw="py-11 px-14 w-full h-full flex flex-col">
@@ -62,11 +61,11 @@ export default async function handler(req: NextRequest) {
             <div
               tw="mt-8 w-full flex justify-center uppercase pt-3"
               style={{
-                color: "#9C57FF",
-                fontSize: "100px",
-                lineHeight: "120px",
-                border: "5.5px solid #F8D966",
-                fontFamily: "Jua",
+                color: '#9C57FF',
+                fontSize: '100px',
+                lineHeight: '120px',
+                border: '5.5px solid #F8D966',
+                fontFamily: 'Jua',
               }}
             >
               {appreciateValue}
@@ -89,9 +88,9 @@ export default async function handler(req: NextRequest) {
                           src={avatar}
                           alt=""
                           style={{
-                            width: "250px",
-                            height: "250px",
-                            background: "#E5DCFF",
+                            width: '250px',
+                            height: '250px',
+                            background: '#E5DCFF',
                           }}
                         />
                       </div>
@@ -112,7 +111,7 @@ export default async function handler(req: NextRequest) {
                       style={{
                         left: -40,
                         top: -30,
-                        background: "#B079FF",
+                        background: '#B079FF',
                       }}
                     >
                       <img
@@ -157,7 +156,7 @@ export default async function handler(req: NextRequest) {
 
             <div
               tw="mt-24 flex text-5xl"
-              style={{ fontFamily: "Jua", lineHeight: "89px" }}
+              style={{ fontFamily: 'Jua', lineHeight: '89px' }}
             >
               {appreciateContent}
             </div>
@@ -169,17 +168,17 @@ export default async function handler(req: NextRequest) {
         height: height,
         fonts: [
           {
-            name: "Jua",
+            name: 'Jua',
             data: font1Data,
-            style: "normal",
+            style: 'normal',
           },
         ],
       }
-    );
+    )
   } catch (e: any) {
-    console.log(`${e.message}`);
+    console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
       status: 500,
-    });
+    })
   }
 }
