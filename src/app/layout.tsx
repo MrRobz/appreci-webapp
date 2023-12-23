8eeb624f60318158dac11431425e5b581a2952af
MrRobz <robinphilipthomas@outlook.in>
2023-12-23 16:26:58 +0530

    u

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,13 +62,13 @@ const trueno = localFont({
 export const metadata: Metadata = {
   title: 'Apperci bot: Employee recognition and celebrations - inside Slack',
   description:
-    'Make employee recognition, celebrations, & rewards fun - all inside Slack. With Appreci, easily send kudos to your teammates and recognize their achievements.',
+    'Make employee recognition, celebrations fun - all inside Slack. With Appreci, easily send kudos to your teammates and recognize their achievements.',
   openGraph: {
     type: 'website',
     url: 'https://apprecibot.com',
     title: 'Apperci bot: Employee recognition and celebrations - inside Slack',
     description:
-      'Make employee recognition, celebrations, & rewards fun - all inside Slack. With Appreci, easily send kudos to your teammates and recognize their achievements.',
+      'Make employee recognition, celebrations fun - all inside Slack. With Appreci, easily send kudos to your teammates and recognize their achievements.',
     images: [
       {
         url: '/assets/meta/meta-hero.png',
@@ -79,7 +79,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Apperci bot: Employee recognition and celebrations - inside Slack',
     description:
-      'Make employee recognition, celebrations, & rewards fun - all inside Slack. With Appreci, easily send kudos to your teammates and recognize their achievements.',
+      'Make employee recognition, celebrations fun - all inside Slack. With Appreci, easily send kudos to your teammates and recognize their achievements.',
     images: '/assets/meta/meta-hero.png',
   },
 };
